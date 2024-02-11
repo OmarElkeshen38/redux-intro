ee9334f4c2bc1db2b6fe998fe1a56786e417d983
omarelkeshen <oelkeshen5@gmail.com>
2024-02-11 19:31:29 +0200

    create a reducer Bank-Account

---
+++ src/store.js
@@ -0,0 +1,21 @@
+const initialState = {
+    balance: 0,
+    loan: 0,
+    loanPurpose: "",
+};
+
+function reducer(state = initialState, action) {
+    switch(action.type) {
+        case "account/deposit":
+            return { ...state, balance: state.balance + action.payload };
+        case "account/withdraw":
+            return { ...state, balance: state.balance - action.payload };
+        case "account/requestLoan":
+            if (state.leon > 0) return state;
+                return { ...state, leon: action.payload };
+        case "account/payLoan":
+            return { ...state, loan: 0, loanPurpose: "", balance: state.balance - state.loan }
+        default:
+            return state;
+    }
+}
